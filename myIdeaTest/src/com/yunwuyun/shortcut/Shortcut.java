b1c08fe1b8d8b24325e63000d9b3a01ff35137b2
fuxin yi <suk178458994@163.com>
2023-08-21 08:13:47 +0800

    feat: idea快捷键

---
+++ myIdeaTest/src/com/yunwuyun/shortcut/Shortcut.java
@@ -0,0 +1,22 @@
+package com.yunwuyun.shortcut;
+
+import java.util.Scanner;
+
+public class Shortcut {
+    public static void main(String[] args) {
+        /**
+         * cmd+1:工程目录的显隐
+         * cmd+4:控制台的显隐
+         * option+cmd+l:格式化代码
+         * option+enter:修复提示
+         * control+r:运行
+         * control+d:debug
+         * shift+cmd+↑/↓:上下移动代码
+         * option+cmd+v 补全返回值
+         */
+        System.out.println("hello world");
+        int a = 1;
+        byte b = (byte)a;
+        System.out.println(b);
+    }
+}
