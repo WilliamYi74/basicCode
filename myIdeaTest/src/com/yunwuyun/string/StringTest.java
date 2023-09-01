e4230b1847a2f494dff91e77e16ae537031b0600
fuxin yi <suk178458994@163.com>
2023-09-01 22:36:57 +0800

    feat: 测试git提交

--- myIdeaTest/src/com/yunwuyun/string/StringTest.java
+++ myIdeaTest/src/com/yunwuyun/string/StringTest.java
@@ -2,7 +2,7 @@ package com.yunwuyun.string;
 
 public class StringTest {
     public static void main(String[] args) {
-        String s = "你好啊";
+        String s = "你好啊java";
         System.out.println(s);
     }
 }
